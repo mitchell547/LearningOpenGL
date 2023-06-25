8740ae7eb64fd6e18e4ca10ada8ac079655f677e
Mikhail <mchalyshev@gmail.com>
2023-06-25 13:13:01 +0300

    refactor main file

--- opengl_lessons/main.cpp
+++ opengl_lessons/main.cpp
@@ -7,52 +7,6 @@
 
 #include "ShaderHelpers.h"
 
-const char *vertexShaderSource = 
-    "#version 330 core\n"
-    "layout (location = 0) in vec3 aPos;\n"
-    "void main()\n"
-    "{\n"
-    "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
-    "}\0";
-
-const char *vertexColorShaderSource =
-    "#version 330 core\n"
-    "layout (location = 0) in vec3 aPos;\n"
-    "layout (location = 1) in vec3 aColor;\n"
-    "out vec3 ourColor;\n"
-    "void main()\n"
-    "{\n"
-    "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
-    "   ourColor = aColor;\n"
-    "}\0";
-
-const char *fragmentShaderSource =
-    "#version 330 core\n"
-    "out vec4 FragColor;\n"
-    "void main()\n"
-    "{\n"
-    "    FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
-    "}\0";
-
-const char *fragmentUniShaderSource =
-    "#version 330 core\n"
-    "out vec4 FragColor;\n"
-    "uniform vec4 ourColor;\n"
-    "void main()\n"
-    "{\n"
-    "    FragColor = ourColor;\n"
-    "}\0";
-
-const char *fragmentColorShaderSource =
-    "#version 330 core\n"
-    "out vec4 FragColor;\n"
-    "in vec3 ourColor;\n"
-    "void main()\n"
-    "{\n"
-    "    FragColor = vec4(ourColor, 1.0);\n"
-    "}\0";
-
-
 void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
     glViewport(0, 0, width, height);
 }
@@ -60,44 +14,6 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
 void processInput(GLFWwindow *window) {
     if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
         glfwSetWindowShouldClose(window, true);
-}
-
-bool checkShader(GLuint shader) {
-    int  success;
-    char infoLog[512];
-    glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
-
-    if (!success)
-    {
-        glGetShaderInfoLog(shader, 512, NULL, infoLog);
-        std::cout << "ERROR::SHADER::COMPILATION_FAILED\n" << infoLog << std::endl;
-        return false;
-    }
-    return true;
-}
-
-bool checkProgram(GLuint program) {
-    int  success;
-    char infoLog[512];
-
-    glGetProgramiv(program, GL_LINK_STATUS, &success);
-    if (!success) {
-        glGetProgramInfoLog(program, 512, NULL, infoLog);
-        std::cout << "ERROR::SHADER::PROGRAM::COMPILATION_FAILED\n" << infoLog << std::endl;
-        return false;
-    }
-    return true;
-}
-
-GLuint makeShader(const GLchar* srcString, GLenum shaderType, GLsizei stringNum = 1)
-{
-    GLuint shader = 0;
-    shader = glCreateShader(shaderType);
-    glShaderSource(shader, stringNum, &srcString, NULL);
-    glCompileShader(shader);
-    if (!checkShader(shader))
-        return 0;
-    return shader;
 }
 
 int main() {    
@@ -125,53 +41,9 @@ int main() {
 
     //
 
-    unsigned int vertexShader = makeShader(vertexShaderSource, GL_VERTEX_SHADER);
-    if (vertexShader <= 0)
-        return -1;
-
-    unsigned int vertexColorShader = makeShader(vertexColorShaderSource, GL_VERTEX_SHADER);
-    if (vertexShader <= 0)
-        return -1;
-
-    unsigned int fragmentShader = makeShader(fragmentShaderSource, GL_FRAGMENT_SHADER);
-    if (vertexShader <= 0)
-        return -1;
-
-    unsigned int fragmentUniShader = makeShader(fragmentUniShaderSource, GL_FRAGMENT_SHADER);
-    if (vertexShader <= 0)
-        return -1;
-
-    unsigned int fragmentColorShader = makeShader(fragmentColorShaderSource, GL_FRAGMENT_SHADER);
-    if (vertexShader <= 0)
-        return -1;
-
-    //
-
-
-    unsigned int shaderProgram = 0;
-    shaderProgram = glCreateProgram();
-    glAttachShader(shaderProgram, vertexShader);
-    //glAttachShader(shaderProgram, fragmentShader);
-    glAttachShader(shaderProgram, fragmentUniShader);
-    glLinkProgram(shaderProgram);
-    if (!checkProgram(shaderProgram))
-        return -1;
-
-    unsigned int shaderColorProgram = 0;
-    shaderColorProgram = glCreateProgram();
-    glAttachShader(shaderColorProgram, vertexColorShader);
-    glAttachShader(shaderColorProgram, fragmentColorShader);
-    glLinkProgram(shaderColorProgram);
-    if (!checkProgram(shaderColorProgram))
-        return -1;
-    
-    glDeleteShader(vertexShader);
-    glDeleteShader(vertexColorShader);
-    glDeleteShader(fragmentShader);
-    glDeleteShader(fragmentUniShader);
-    glDeleteShader(fragmentColorShader);
-
+    ShaderProgram uniformShaderProgram("..\\Shaders\\basicVertexShader.vert", "..\\Shaders\\uniformColorShader.frag");
     ShaderProgram myShaderProgram("..\\Shaders\\basicVertexShader.vert", "..\\Shaders\\basicFragmentShader.frag");
+    ShaderProgram colorShaderProgram("..\\Shaders\\vertexColorShader.vert", "..\\Shaders\\customColorShader.frag");
 
     //
 
@@ -234,20 +106,20 @@ int main() {
 
     //
 
-
-    //
-
-    //glUseProgram(shaderProgram);
-    //glUseProgram(shaderColorProgram);
-    glUseProgram(myShaderProgram.getID());
+    glUseProgram(uniformShaderProgram.getID());
+    //glUseProgram(myShaderProgram.getID());
+    //glUseProgram(colorShaderProgram.getID());
     glBindVertexArray(VAO);
     
-
     //
 
     glViewport(0, 0, 800, 600);
 
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    /*GLuint shaders[3] = { uniformShaderProgram.getID() , myShaderProgram.getID() , colorShaderProgram.getID() };
+    int frameTimer = 0;
+    int curShader = 0;*/
 
     while (!glfwWindowShouldClose(window))
     {
@@ -256,11 +128,19 @@ int main() {
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
         
-        /*float timeValue = glfwGetTime();
+        float timeValue = glfwGetTime();
         float greenValue = sin(timeValue) / 2.0f + 0.5f;
-        int vertexColorLocation = glGetUniformLocation(shaderProgram, "ourColor");
+        int vertexColorLocation = glGetUniformLocation(uniformShaderProgram.getID(), "ourColor");
         glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);
-*/
+
+        /*frameTimer++;
+        if (frameTimer >= 5000)
+        {
+            frameTimer = 0;
+            curShader = (curShader + 1) % (sizeof(shaders) / sizeof(GLuint));
+        }
+        glUseProgram(shaders[curShader]);*/
+
         glDrawArrays(GL_TRIANGLES, 0, 3);
         //glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
@@ -271,8 +151,6 @@ int main() {
     glDeleteVertexArrays(1, &VAO);
     glDeleteBuffers(1, &VBO);
     glDeleteBuffers(1, &EBO);
-    glDeleteProgram(shaderProgram);
-    glDeleteProgram(shaderColorProgram);
 
     glfwTerminate();
 
