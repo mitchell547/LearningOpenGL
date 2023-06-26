8ca3a4b2710b1f76ac62762c27513ddf6722cfe0
Mikhail <mchalyshev@gmail.com>
2023-06-27 00:26:59 +0300

    add code for texture drawing

--- Shaders/vertexColorShader.vert
+++ Shaders/vertexColorShader.vert
@@ -9,4 +9,5 @@ void main()
 {
    gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);
    ourColor = aColor;
+   //ourColor = aPos;
 }
--- opengl_lessons/main.cpp
+++ opengl_lessons/main.cpp
@@ -1,6 +1,8 @@
 
 #include <glad/glad.h> 
 #include <GLFW/glfw3.h>
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
 
 #include <iostream>
 //#include <filesystem>
@@ -15,6 +17,9 @@ void processInput(GLFWwindow *window) {
     if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
         glfwSetWindowShouldClose(window, true);
 }
+
+const std::string SHADERS_ROOT = "..\\Shaders";
+const std::string TEXTURES_ROOT = "..\\Textures";
 
 int main() {    
     //std::cout << std::experimental::filesystem::current_path() << std::endl;
@@ -41,32 +46,43 @@ int main() {
 
     //
 
-    ShaderProgram uniformShaderProgram("..\\Shaders\\basicVertexShader.vert", "..\\Shaders\\uniformColorShader.frag");
-    ShaderProgram myShaderProgram("..\\Shaders\\basicVertexShader.vert", "..\\Shaders\\basicFragmentShader.frag");
-    ShaderProgram colorShaderProgram("..\\Shaders\\vertexColorShader.vert", "..\\Shaders\\customColorShader.frag");
+    ShaderProgram uniformShaderProgram(SHADERS_ROOT + "\\basicVertexShader.vert", SHADERS_ROOT + "\\uniformColorShader.frag");
+    ShaderProgram myShaderProgram(SHADERS_ROOT + "\\basicVertexShader.vert", SHADERS_ROOT + "\\basicFragmentShader.frag");
+    ShaderProgram colorShaderProgram(SHADERS_ROOT + "\\vertexColorShader.vert", SHADERS_ROOT + "\\customColorShader.frag");
+    ShaderProgram textureShaderProgram(SHADERS_ROOT + "\\textureShader.vert", SHADERS_ROOT + "\\textureShader.frag");
 
     //
 
-    /*float vertices[] = {
+    /*float triVertices[] = {
         -0.5f, -0.5f, 0.0f,
          0.5f, -0.5f, 0.0f,
          0.0f,  0.5f, 0.0f
     };*/
-    /*float vertices[] = {
-     0.5f,  0.5f, 0.0f,  // top right
-     0.5f, -0.5f, 0.0f,  // bottom right
-    -0.5f, -0.5f, 0.0f,  // bottom left
-    -0.5f,  0.5f, 0.0f   // top left 
-    };*/
+    
 
-    float vertices[] = {
+    float triVertices[] = {
         // positions         // colors
          0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,   // bottom right
         -0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,   // bottom left
          0.0f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f    // top 
     };
 
-    unsigned int indices[] = {  
+    //float quadVertices[] = {
+    // 0.5f,  0.5f, 0.0f,  // top right
+    // 0.5f, -0.5f, 0.0f,  // bottom right
+    //-0.5f, -0.5f, 0.0f,  // bottom left
+    //-0.5f,  0.5f, 0.0f   // top left
+    //};
+
+    float quadVertices[] = {
+        // positions          // colors           // texture coords
+         0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f,   // top right
+         0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f,   // bottom right
+        -0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f,   // bottom left
+        -0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f    // top left 
+    };
+
+    unsigned int quadIndices[] = {  
         0, 1, 3,   // first triangle
         1, 2, 3    // second triangle
     };
@@ -85,32 +101,72 @@ int main() {
     glBindVertexArray(VAO);
 
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    //glBufferData(GL_ARRAY_BUFFER, sizeof(triVertices), triVertices, GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(quadVertices), quadVertices, GL_STATIC_DRAW);
     
     // position attribute
     //glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
     //glEnableVertexAttribArray(0);
 
+    //// position attribute
+    //glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+    //glEnableVertexAttribArray(0);
+    ////// color attribute
+    //glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+    //glEnableVertexAttribArray(1);
+
     // position attribute
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
-    // color attribute
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+    //// color attribute
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
     glEnableVertexAttribArray(1);
 
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+    glEnableVertexAttribArray(2);
+
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(quadIndices), quadIndices, GL_STATIC_DRAW);
 
     glBindBuffer(GL_ARRAY_BUFFER, 0);
     glBindVertexArray(0);
 
     //
 
-    glUseProgram(uniformShaderProgram.getID());
+    unsigned int texture;
+    glGenTextures(1, &texture);
+
+    glBindTexture(GL_TEXTURE_2D, texture);
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    
+    int width, height, nrChannels;
+    unsigned char *data = stbi_load((TEXTURES_ROOT + "\\wall.jpg").c_str(), &width, &height, &nrChannels, 0);
+
+    if (data)
+    {
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    }
+    else
+    {
+        std::cout << "Failed to load texture" << std::endl;
+        return -1;
+    }
+
+    stbi_image_free(data);
+
+    //
+
+    //glUseProgram(uniformShaderProgram.getID());
     //glUseProgram(myShaderProgram.getID());
     //glUseProgram(colorShaderProgram.getID());
+    glUseProgram(textureShaderProgram.getID());
     glBindVertexArray(VAO);
-    
+    glBindTexture(GL_TEXTURE_2D, texture);
     //
 
     glViewport(0, 0, 800, 600);
@@ -120,7 +176,7 @@ int main() {
     /*GLuint shaders[3] = { uniformShaderProgram.getID() , myShaderProgram.getID() , colorShaderProgram.getID() };
     int frameTimer = 0;
     int curShader = 0;*/
-
+    
     while (!glfwWindowShouldClose(window))
     {
         processInput(window);
@@ -131,18 +187,18 @@ int main() {
         float timeValue = glfwGetTime();
         float greenValue = sin(timeValue) / 2.0f + 0.5f;
         int vertexColorLocation = glGetUniformLocation(uniformShaderProgram.getID(), "ourColor");
-        glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);
+        //glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);
 
         /*frameTimer++;
         if (frameTimer >= 5000)
         {
             frameTimer = 0;
-            curShader = (curShader + 1) % (sizeof(shaders) / sizeof(GLuint));
+            curShader = (curShader + 1) % (sizeof(shaders) / sizeof(shaders[0]));
         }
         glUseProgram(shaders[curShader]);*/
 
-        glDrawArrays(GL_TRIANGLES, 0, 3);
-        //glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        //glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
         glfwSwapBuffers(window);
         glfwPollEvents();
